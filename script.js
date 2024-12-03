c16467c562c47c21c46bf076e35dbd710accd64e
g013r <hectorsegundo23@gmail.com>
2024-12-02 23:02:23 -0500

    porcentaje dinamico

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ document.querySelectorAll('.flip-box').forEach((box) => {
     const circularProgress = box.querySelector('.circular-progress');
     const label = circularProgress.getAttribute('data-label');
     const color = circularProgress.getAttribute('data-color');
-    const porcentaje = circularProgress.getAttribute('porcentaje')
-    const porcentaje_menor = circularProgress.getAttribute('porcentaje-menor') 
+    const porcentaje = circularProgress.getAttribute('data-porcentaje')
 
     circularProgress.style.setProperty('--color', color);
     circularProgress.setAttribute('data-label', label);
-    circularProgress.getAttribute('porcentaje', porcentaje)
-    circularProgress.getAttribute('porcentaje-menor', porcentaje_menor)
-
+    circularProgress.style.setProperty('--data-porcentaje', porcentaje)
 });
